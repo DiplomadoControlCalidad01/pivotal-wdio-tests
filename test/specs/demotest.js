6279329d826b5e828650963933fc1cd303650d5d
henry-benito-h <henry.benito.h@gmail.com>
2019-02-01 20:25:14 -0400

    Adding support for browser stack

--- test/specs/demotest.js
+++ test/specs/demotest.js
@@ -7,11 +7,6 @@ const login= require('../pages/login.po');
 
 describe('webdriver.io page', () => {
 
-    it('should have the right title - the fancy generator way', () => {
-        browser.url('https://www.pivotaltracker.com/signin');
-        var title = browser.getTitle();
-        assert.equal(title, 'Pivotal Tracker - Sign in');
-    });
     it('Login to pivotal tracker', () => {
         let dashboard = login.loginAs(credentials.sysadmin.username, credentials.sysadmin.password);
         let createProjectModal = dashboard.clickCreateProjectButton();
