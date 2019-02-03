4d44e8dd31c541f243f09b4aead94fe583f28183
henry-benito-h <henry.benito.h@gmail.com>
2019-02-03 05:02:31 -0400

    Adding support for docker

--- test/core/CommonActions.js
+++ test/core/CommonActions.js
@@ -1,23 +1,29 @@
+const {explicit} = require('../../environment').timeout;
+
 class CommonActions {
     static setValue(locator, value) {
-        browser.waitForVisible(locator, 30000);
+        browser.waitForVisible(locator, explicit);
         browser.element(locator).setValue(value);
     }
 
     static click(locator) {
-        browser.waitForVisible(locator, 30000);
+        browser.waitForVisible(locator, explicit);
         browser.element(locator).click();
     }
 
     static waitForVisible(locator) {
+<<<<<<< HEAD
         return browser.waitForVisible(locator, 30000);
+=======
+        browser.waitForVisible(locator, explicit);
+>>>>>>> Adding support for docker
     }
     static waitForInvisible(locator) {
-        browser.waitForVisible(locator, 30000, true);
+        browser.waitForVisible(locator, explicit, true);
     }
 
     static getText(locator) {
-        browser.waitForVisible(locator, 30000);
+        browser.waitForVisible(locator, explicit);
         return browser.getText(locator);
     }
 
